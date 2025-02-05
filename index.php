af57a5e5a5eb4df0ad6a0a24e49664e288be334d
naxymails@gmail.com <naxymails@gmail.com>
2025-02-05 18:04:06 +0300

    fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $APPLICATION->SetTitle("Мебельная компания");
 ?><p>
 Это изменение добавленное уже на локальной машине в ветке main
 </p><p>
-<p>ССылка на репозиторий <a href="https://github.com/naysy-sx/kss-server/tree/main">kss-server</a></p>
+<h1 style="color: red;">ССылка на репозиторий <a href="https://github.com/naysy-sx/kss-server/tree/main">kss-server</a></h1>
 <h3>И снова здравствуйте</h3>
 <?$APPLICATION->IncludeComponent("bitrix:furniture.catalog.index", "", array(
 	"IBLOCK_TYPE" => "products",
